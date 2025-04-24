d1a0ffc4d9f79798472177cbcdd42ef50d4d6d2d
zulfiqarali375 <zulfiqarbrohi375@gmail.com>
2025-04-24 14:59:51 +0500

    changes

--- src/components/LoadMore.jsx
+++ src/components/LoadMore.jsx
@@ -3,14 +3,15 @@ import React from "react";
 function LoadMore() {
   return (
     <div>
-      <div class="text-center">
-        <button
-          type="button"
-          class="py-2.5 px-5 me-2 mt-16 hover:bg-black text-md font-medium bg-gray-300 h-11 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          LOAD MORE
-        </button>
-      </div>
+<div class="flex justify-center">
+  <button
+    type="button"
+    class="py-2.5 px-5 me-2 mt-16 text-md font-medium h-11 focus:outline-none rounded-full border border-gray-200 bg-gray-300 hover:bg-black hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+  >
+    LOAD MORE
+  </button>
+</div>
+
     </div>
   );
 }
