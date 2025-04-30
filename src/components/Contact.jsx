05932c1cb65c5534faafb9769d1c0c9fb042456a
zulfiqarali375 <zulfiqarbrohi375@gmail.com>
2025-04-30 16:09:02 +0500

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ function Contact() {
 
 
       <section className="text-gray-600 body-font  relative">
-        <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:w-[90%] sm:mr-10 p-10 flex items-end justify-start relative">
             <iframe
               width="100%"
